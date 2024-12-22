17affa7b1bf282728c31f1556af68623310d96c5
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2024-12-22 13:24:46 -0800

    feat: fluid tank and gas canister options button

--- packs/BP/scripts/index.ts
+++ packs/BP/scripts/index.ts
@@ -6,4 +6,16 @@ import "./tutorial_book";
 
 becInit("fluffyalien_energistics");
 
+//TODO: disposal unit recipe
+//TODO: disposal unit tutorial book entry
+//TODO: battery recipe
+//TODO: battery tutorial book entry
+//TODO: fluid tank recipe
+//TODO: fluid tank tutorial book entry
+//TODO: gas canister recipe
+//TODO: gas canister tutorial book entry
+//TODO: pump recipe
+//TODO: pump tutorial book entry
+
+//TODO: fix storage type container storage bar flashing
 //TODO: machine balancing (oil generator is way too OP)
--- packs/BP/scripts/machines/shared/generic_storage_type_container.ts
+++ packs/BP/scripts/machines/shared/generic_storage_type_container.ts
@@ -1,4 +1,4 @@
-import { BlockCustomComponent } from "@minecraft/server";
+import { BlockCustomComponent, Player, system, world } from "@minecraft/server";
 import { BlockStateAccessor } from "../../utils/block";
 import {
   generate,
@@ -6,10 +6,15 @@ import {
   MachineCallbackArg,
   MachineDefinition,
   MachineDefinitionDescription,
+  MachineDefinitionEvents,
   MachineDefinitionHandlers,
+  MachineOnButtonPressedEventArg,
   MachineRecieveHandlerArg,
+  STANDARD_STORAGE_TYPE_DEFINITIONS,
+  StandardStorageType,
   UpdateUiHandlerResponse,
 } from "bedrock-energistics-core-api";
+import { ModalFormData } from "@minecraft/server-ui";
 
 const AMOUNT_PER_STAGE = 2500;
 const MAX_STORAGE = AMOUNT_PER_STAGE * 3;
@@ -18,7 +23,7 @@ const STAGE_AMOUNT_PADDING = 200;
 export class GenericStorageTypeContainerMachine implements MachineDefinition {
   constructor(
     readonly id: string,
-    readonly acceptedTypes: string[],
+    readonly acceptedTypes: StandardStorageType[],
   ) {}
 
   get description(): MachineDefinitionDescription {
@@ -31,6 +36,13 @@ export class GenericStorageTypeContainerMachine implements MachineDefinition {
             type: "storageBar",
             startIndex: 0,
           },
+          optionsButton: {
+            type: "button",
+            index: 4,
+            defaults: {
+              itemId: "fluffyalien_energistics:ui_options_button",
+            },
+          },
         },
       },
     };
@@ -40,6 +52,14 @@ export class GenericStorageTypeContainerMachine implements MachineDefinition {
     return {
       updateUi: this.updateUi.bind(this),
       receive: this.receive.bind(this),
+    };
+  }
+
+  get events(): MachineDefinitionEvents {
+    return {
+      onButtonPressed: (e): void => {
+        void this.onButtonPressed(e);
+      },
     };
   }
 
@@ -61,7 +81,7 @@ export class GenericStorageTypeContainerMachine implements MachineDefinition {
   }
 
   receive(e: MachineRecieveHandlerArg): number | undefined {
-    if (!this.acceptedTypes.includes(e.receiveType)) return 0;
+    if (!(this.acceptedTypes as string[]).includes(e.receiveType)) return 0;
 
     const block = e.blockLocation.dimension.getBlock(e.blockLocation);
     if (!block) return 0;
@@ -75,10 +95,53 @@ export class GenericStorageTypeContainerMachine implements MachineDefinition {
 
     if (type === "none") {
       typeState.set(e.receiveType);
-      return undefined;
+      return;
     }
 
     if (type !== e.receiveType) return 0;
+  }
+
+  async onButtonPressed(e: MachineOnButtonPressedEventArg): Promise<void> {
+    const block = e.blockLocation.dimension.getBlock(e.blockLocation);
+    if (!block) return;
+
+    world.getEntity(e.entityId)?.remove();
+
+    await system.waitTicks(4);
+
+    const typeState = new BlockStateAccessor<StandardStorageType | "none">(
+      block,
+      "fluffyalien_energistics:type",
+    );
+
+    const type = typeState.get();
+
+    const currentIndex = type === "none" ? 0 : this.acceptedTypes.indexOf(type);
+
+    const form = new ModalFormData()
+      .title({
+        translate: `tile.${this.id}.name`,
+      })
+      .dropdown(
+        {
+          translate:
+            "fluffyalien_energistics.ui.genericStorageTypeContainer.options.type",
+        },
+        this.acceptedTypes.map(
+          (acceptedType) =>
+            STANDARD_STORAGE_TYPE_DEFINITIONS[acceptedType].name,
+        ),
+        currentIndex,
+      );
+
+    const player = world.getEntity(e.playerId) as Player;
+
+    const response = await form.show(player);
+    if (!response.formValues) return;
+
+    const selectedIndex = response.formValues[0] as number;
+
+    typeState.set(this.acceptedTypes[selectedIndex]);
   }
 }
 

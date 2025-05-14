d90f7cb862099c79303a7259bfd1ee26bccd23e9
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2025-05-13 18:23:47 -0700

    fix: block uid not actually unique

--- packs/BP/scripts/utils/location.ts
+++ packs/BP/scripts/utils/location.ts
@@ -3,8 +3,11 @@ import { DimensionLocation, Entity } from "@minecraft/server";
 export function blockLocationToUid(location: DimensionLocation): string {
   return (
     location.dimension.id +
+    "," +
     location.x.toString() +
+    "," +
     location.y.toString() +
+    "," +
     location.z.toString()
   );
 }
